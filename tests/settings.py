dadd74e8b7d01d800ad3100836a0f1706bbe171a
Raphael Michel <michel@rami.io>
2026-03-05 20:28:16 +0100

    Fix postgres data

--- tests/settings.py
+++ tests/settings.py
@@ -48,8 +48,8 @@ if os.environ['TOXDB'] == 'postgres':
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'testapp',
-            'USER': 'raphael',
-            'PASSWORD': '',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
             'HOST': 'localhost'
         }
     }
